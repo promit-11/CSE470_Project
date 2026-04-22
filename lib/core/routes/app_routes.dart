c91898a1ac464035dbcc9ffb0e3814d1a7f267e1
Asiful Hasan <124084724+imgazi@users.noreply.github.com>
2026-04-22 07:14:37 +0600

    app routes file

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,19 @@
+class AppRoutes {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String roleHome = '/home';
+  static const String studentDashboard = '/student/dashboard';
+  static const String studentProfile = '/student/profile';
+  static const String studentAnalytics = '/student/analytics';
+  static const String studentArchive = '/student/archive';
+  static const String studentCoachingAssignment =
+      '/student/coaching-assignment';
+  static const String studentExam = '/student/exam';
+  static const String studentResult = '/student/result';
+  static const String teacherPendingApproval = '/teacher/pending-approval';
+  static const String teacherDashboard = '/teacher/dashboard';
+  static const String teacherReviewDetail = '/teacher/review-detail';
+  static const String coachingDashboard = '/coaching/dashboard';
+  static const String adminExams = '/admin/exams';
+}
